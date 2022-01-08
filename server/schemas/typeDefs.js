1d44f4eaa3ef6abe060f5b9f3d08b41e6f46b253
Blake McCarty <blake_mccarty@yahoo.com>
2022-01-08 13:40:53 -0600

    heroku deploy

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,25 +7,25 @@ const typeDefs = gql`
     lastname: String!
     username: String!
     email: String!
-    focuses:[focus]
-    sparks:[spark]
-    friends:[user]
+    focuses: [Focus]
+    sparks: [Spark]
+    friends: [User]
   }
   type Auth {
     token: ID!
     user: User!
   }
-  type Focus{
-    _id; ID
-    title:String!
-    description:String
-     sparks:[spark]
+  type Focus {
+    _id: ID
+    title: String!
+    description: String
+    sparks: [Spark]
   }
   type Spark {
-    _id:ID
-    title:String!
-    description:String!
-     sparks:[spark]
+    _id: ID
+    title: String!
+    description: String!
+    sparks: [Spark]
   }
   type Query {
     users: [User]!
@@ -39,23 +39,18 @@ const typeDefs = gql`
       email: String!
       password: String!
     ): Auth
-    addFocus(
-      _id: ID
-      title: String!
-      description:String!
-    )
-    addSpark(
-      _id: ID
-      title: String!
-      description:String!
-    )
+
+    addFocus(_id: ID, title: String!, description: String!): Auth
+
+    addSpark(_id: ID, title: String!, description: String!): Auth
+
     addFriend(
-    _id: ID
-    firstname: String!
-    lastname: String!
-    username: String!
-    email: String!
-    )
+      _id: ID
+      firstname: String!
+      lastname: String!
+      username: String!
+      email: String!
+    ): Auth
     login(email: String!, password: String!): Auth
     removeUser: User
   }
