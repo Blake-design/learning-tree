6607cfb79ed7e940f869be89bf59f2a3722597d4
Blake McCarty <blake_mccarty@yahoo.com>
2022-01-16 00:53:48 -0600

    update manifest icons

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../../App.css";
-
 import Auth from "../../utils/auth";
 // import "./header.css";
 
@@ -13,33 +12,38 @@ const Header = () => {
 
   const navigate = useNavigate();
 
-
   return (
     <header>
       <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
         <Link className="text-dark" to="/">
-          <h3 className="m-0" >
-            Synapse
-          </h3>
+          <h3 className="m-0">Synapse</h3>
         </Link>
         {/* <p>
           Pathways to learning
         </p> */}
       </div>
       <div>
-          {Auth.loggedIn() ? (
-            <>
-              {/* <Link className="header-btn" to="/create">Create</Link> */}
-              <button className="header-btn" onClick={() => navigate("/create")}>Create</button>
-              <button className="header-btn" onClick={logout}>Logout</button>
-            </>
-          ) : (
-            <>
-              <button className="header-btn" onClick={() => navigate("/login")}>Log In</button>
-              <button className="header-btn" onClick={() => navigate("/signup")}>Sign Up</button>
-            </>
-          )}
-        </div>
+        {Auth.loggedIn() ? (
+          <>
+            {/* <Link className="header-btn" to="/create">Create</Link> */}
+            <button className="header-btn" onClick={() => navigate("/create")}>
+              Create
+            </button>
+            <button className="header-btn" onClick={logout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <button className="header-btn" onClick={() => navigate("/login")}>
+              Log In
+            </button>
+            <button className="header-btn" onClick={() => navigate("/signup")}>
+              Sign Up
+            </button>
+          </>
+        )}
+      </div>
     </header>
   );
 };
