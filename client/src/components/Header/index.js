26dea24dec6da2cf718849c1d318414493cebd9f
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2022-01-18 12:06:06 -0600

    Moved JWT secret to .env file

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -48,11 +48,11 @@ const Header = () => {
     // </header>
 <header>
   <nav className="navbar" role="navigation" aria-label="main navigation">
+
     <div className="navbar-brand">
       <a className="navbar-item" href="https://bulma.io">
         <img src={synapseLogo} width="40" height="40"/>
       </a>
-
       <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
@@ -61,14 +61,14 @@ const Header = () => {
     </div>
 
     <div id="navbarBasicExample" className="navbar-menu">
-      <div className="navbar-start">
+      {/* <div className="navbar-start">
         <a>
           Home
         </a>
 
         <a className="navbar-item">
           Documentation
-        </a>
+        </a> */}
 
         {/* <div className="navbar-item has-dropdown is-hoverable">
           <a className="navbar-link">
@@ -91,7 +91,7 @@ const Header = () => {
             </a>
           </div>
         </div> */}
-      </div>
+      {/* </div> */}
 
       <div className="navbar-end">
         <div className="navbar-item">
@@ -105,7 +105,9 @@ const Header = () => {
           {/* </div> */}
         </div>
       </div>
+
     </div>
+    
   </nav>
 </header>
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
+// require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-const secret = "mysecretssshhhhhhh";
 const expiration = "2h";
 
 module.exports = {
@@ -19,7 +19,7 @@ module.exports = {
 
     // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(token, `${process.env.SECRET}`, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log("Invalid token");
@@ -31,6 +31,6 @@ module.exports = {
   signToken: function ({ firstName, lastName, userName, email }) {
     const payload = { firstName, lastName, userName, email };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign({ data: payload }, `${process.env.SECRET}`, { expiresIn: expiration });
   },
 };
