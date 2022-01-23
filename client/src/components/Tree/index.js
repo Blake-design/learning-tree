b14a703292f6628277cb114380a842a194527ff7
Blake McCarty <blake_mccarty@yahoo.com>
2022-01-23 14:11:13 -0600

    added color to nodes

--- client/src/components/Tree/index.js
+++ client/src/components/Tree/index.js
@@ -22,12 +22,18 @@ const OrgChartTree = ({ user }) => {
     }
     return newNode;
   }
+
   // console.log(convertedD3Obj);
   return (
     // <div id="treeWrapper" style={{ width: "50em", height: "20em" }}>
     <div id="treeWrapper">
       {user.sparks.length ? (
-        <Tree data={convertedD3Obj} />
+        <Tree
+          data={convertedD3Obj}
+          rootNodeClassName="node__root"
+          branchNodeClassName="node__branch"
+          leafNodeClassName="node__leaf"
+        />
       ) : (
         <h4 id="nodata">You currently have no data.</h4>
       )}
